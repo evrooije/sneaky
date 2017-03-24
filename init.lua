209be0b6f8aac2d31a4ac8e70baed6be9c21ac8d
evrooije <evrooije@outlook.com>
2017-03-24 17:41:31 +0100

    Reverting changes as they broke the mod

--- init.lua
+++ init.lua
@@ -1,57 +1,39 @@
 --[[
-
 Beerholder's sneaky ladder. WIP name: [MOD] Sneaky [sneaky]
-
 To sneak climb we need to consider either something, then air above
 it and then something again, or air, something and air above it.
 This pattern needs to be anywhere next to the player.
-
 Diagrammatic climb ("x" something " " air "p" player):
-
  2  x         2
  1     p  OR  1  x
  0  x         0     p
-
 To sneak hover we need to consider air below and then something on top.
-
 Diagrammatic hover (x something a air p player):
-
  0  x  p
 -1
 -2  x
-
 We use a transparent (airlike) dummy node to stand/ hover on. We set
 the selection box to something ridiculously small so one cannot see it.
 It looks something like this ("s" = the sneaky block, sneakily hiding
 underneath you):
-
  0  x  p
 -1     s
 -2  x
 -3
 -4  x
 -5
-
 ]]--
 
 -- Register the sneaky node to place underneath the player
 minetest.register_node("sneaky:sneaky", {
     drawtype = "airlike",
+    groups = { fall_damage_add_percent = -100 },
     selection_box = {
         type = "fixed",
 		fixed = {
             {-0.00001, -0.00001, -0.00001, 0.00001, 0.00001, 0.00001}
         }
-    },
-    groups = { fall_damage_add_percent = -100 },
-    sunlight_propagates = true,
-    buildable_to = true,
-    after_place_node = function(pos, placer, itemstack, pointed_thing)
-        minetest.get_node_timer(pos):start(60)
-    end,
-    on_timer = function(pos, elapsed)
-        return false
-    end
+    }
 })
 
 -- Check for key presses sneak + jump or sneak only and check if there
@@ -62,13 +44,7 @@ minetest.register_globalstep(function(dtime)
         return
     end
 
-    -- Get the positions above, below and at the player, needed to do
-    -- sneaky checks, sneaky creation and sneaky removal
     local pos = { x = player:getpos().x, y = player:getpos().y, z = player:getpos().z }
-    local pos2above = { x = pos.x, y = pos.y + 2, z = pos.z }
-    local node2above = minetest.get_node(pos2above)
-    local pos1above = { x = pos.x, y = pos.y + 1, z = pos.z }
-    local node1above = minetest.get_node(pos1above)
     local pos0below = { x = pos.x, y = pos.y, z = pos.z }
     local node0below = minetest.get_node(pos0below)
     local pos1below = { x = pos.x, y = pos.y - 1, z = pos.z }
@@ -76,7 +52,6 @@ minetest.register_globalstep(function(dtime)
     local pos2below = { x = pos.x, y = pos.y - 2, z = pos.z }
     local node2below = minetest.get_node(pos2below)
 
-    -- If we sneak and jump, we go try do the ladder thing
     if player:get_player_control().sneak and player:get_player_control().jump then
 
         -- Table of positions we are interested in around us
@@ -95,7 +70,7 @@ minetest.register_globalstep(function(dtime)
         local pos2right = { x = pos.x + 1, y = pos.y + 2, z = pos.z }
         local pos2left = { x = pos.x - 1, y = pos.y + 2, z = pos.z }
 
-        -- Nodes around us
+        -- Nodes around is
         local node0front = minetest.get_node(pos0front)
         local node0back = minetest.get_node(pos0back)
         local node0right = minetest.get_node(pos0right)
@@ -135,16 +110,7 @@ minetest.register_globalstep(function(dtime)
 
         if sneakyclimb then
             player:set_physics_override( { gravity = 0, jump = 0 } )
-            player:setpos( { x = player:getpos().x, y = player:getpos().y + 0.25, z = player:getpos().z } )
-            if (node2above.name == "sneaky:sneaky") then
-                minetest.remove_node(pos2above)
-            end
-            if (node1above.name == "sneaky:sneaky") then
-                minetest.remove_node(pos1above)
-            end
-            if (node0below.name == "sneaky:sneaky") then
-                minetest.remove_node(pos0below)
-            end
+            player:setpos( { x = player:getpos().x, y = player:getpos().y + 0.2, z = player:getpos().z } )
             if (node1below.name == "air") then
                 minetest.place_node(pos1below, { name = "sneaky:sneaky" } )
             end
@@ -155,10 +121,8 @@ minetest.register_globalstep(function(dtime)
         end
     end
 
-    -- If we sneak, we go try do the hover thing
     if player:get_player_control().sneak then
 
-        -- Only get two layers this time
         local pos0front = { x = pos.x, y = pos.y - 1, z = pos.z + 1 }
         local pos0back = { x = pos.x, y = pos.y - 1, z = pos.z + -1 }
         local pos0right = { x = pos.x + 1, y = pos.y - 1, z = pos.z }
@@ -194,20 +158,11 @@ minetest.register_globalstep(function(dtime)
 
         if sneakyhover then
             player:set_physics_override( { gravity = 0, jump = 1 } )
-            if (node2above.name == "sneaky:sneaky") then
-                minetest.remove_node(pos2above)
-            end
-            if (node1above.name == "sneaky:sneaky") then
-                minetest.remove_node(pos1above)
-            end
-            if (node0below.name == "sneaky:sneaky") then
-                minetest.remove_node(pos0below)
-            end
             if (node1below.name == "air") then
                 minetest.place_node(pos1below, { name = "sneaky:sneaky" } )
             end
-            if (node2below.name == "air") then
-                minetest.place_node(pos2below, { name = "sneaky:sneaky" } )
+            if (node2below.name == "sneaky:sneaky") then
+                minetest.remove_node(pos2below)
             end
             return
         end
@@ -215,15 +170,8 @@ minetest.register_globalstep(function(dtime)
 
     -- Reset the original settings. Better implementation is obviously
     -- to backup the original settings and restore them ... Also remove
-    -- sneaky nodes so that the player will start falling and cleanup
-    -- any leftover sneaky nodes above the player as well
+    -- sneaky nodes so that the player will start falling
     player:set_physics_override( { gravity = 1, jump = 1 } )
-    if (node2above.name == "sneaky:sneaky") then
-        minetest.remove_node(pos2above)
-    end
-    if (node1above.name == "sneaky:sneaky") then
-        minetest.remove_node(pos1above)
-    end
     if (node0below.name == "sneaky:sneaky") then
         minetest.remove_node(pos0below)
     end
